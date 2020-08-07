533ae90369432f28e8d5e5d7a7961f910b28e2b7
Bogdan Deshko <bodiath@gmail.com>
2020-08-07 17:35:52 +0300

    New prroject

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Bogdan Deshko on 8/7/20.
+//  Copyright © 2020 JetSoftPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
