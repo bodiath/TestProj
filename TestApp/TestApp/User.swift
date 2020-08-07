1d8187f344c8751996755ed9e98426d19d5243d0
AllenMayer <varhalla@ukr.net>
2020-08-07 18:42:38 +0300

    create user model

---
+++ TestApp/TestApp/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  TestApp
+//
+//  Created by Максим Семений on 07.08.2020.
+//  Copyright © 2020 JetSoftPro. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let age: Int
+}
