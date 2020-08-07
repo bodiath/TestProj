f0762a3b271870367b28b3a0ed2cbe942d6722ea
Bogdan Deshko <bodiath@gmail.com>
2020-08-07 18:46:21 +0300

    add login

---
+++ TestApp/TestApp/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  TestApp
+//
+//  Created by Bogdan Deshko on 8/7/20.
+//  Copyright © 2020 JetSoftPro. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
