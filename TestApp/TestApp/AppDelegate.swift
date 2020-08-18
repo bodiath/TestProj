d52211bfe3c52772b73bc319b0df1f1cc06d4abc
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-18 19:13:26 +0300

    save changes

--- TestApp/TestApp/AppDelegate.swift
+++ TestApp/TestApp/AppDelegate.swift
@@ -10,9 +10,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    let arr = 6
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     let task = 5
+    let task2 = 5
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = URL(fileURLWithPath: "www.jsp.com")
